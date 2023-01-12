a8ba74b56f250167261eaa5c1c0ca8013a62e12b
javiergranadosr <javiergranadosrdev@gmail.com>
2023-01-11 22:25:29 -0600

    Swagger
    Se agrega configuracion en swagger para documentacion.

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+    private ApiInfo apiInfo() {
+        return new ApiInfo("Sistema productos Rest APIs",
+                "APIs Sistema productos.",
+                "1.0",
+                "",
+                new Contact("", "", ""),
+                "",
+                "",
+                Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+                .paths(PathSelectors.any()) // Todos los controladores del paquete
+                .build();
+    }
+}
